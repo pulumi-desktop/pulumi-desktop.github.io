e290dd35717eb9c472e35467c139c8d0bbe26d6c
Praneet <1466314+praneetloke@users.noreply.github.com>
2021-12-30 23:59:35 -0500

    Get the latest version from GH releases API and set the button href on page load

---
+++ src/main.ts
@@ -0,0 +1,64 @@
+interface ReleaseAsset {
+    created_at: string;
+    updated_at: string;
+    browser_download_url: string;
+    name: string;
+}
+
+interface GitHubRelease {
+    url: string;
+    tag_name: string;
+    assets: ReleaseAsset[];
+}
+
+let latestRelease: GitHubRelease | undefined;
+
+async function getLatestVersion() {
+    const resp = await fetch(
+        "https://api.github.com/repos/pulumi-desktop/app/releases/latest"
+    );
+    latestRelease = await resp.json();
+}
+
+function enableDownloadButtons() {
+    const buttons = document.getElementsByClassName("download-btn");
+    for (let i = 0; i < buttons.length; i++) {
+        const btn = buttons[i] as HTMLAnchorElement;
+        const os = btn.dataset.os;
+        let releaseAsset: ReleaseAsset | undefined;
+        switch (os) {
+            case "linux":
+                releaseAsset = latestRelease.assets.find((a) =>
+                    a.name.includes("snap")
+                );
+                console.log("releaseAsset for", os, releaseAsset);
+                break;
+            case "darwin":
+                releaseAsset = latestRelease.assets.find((a) =>
+                    a.name.includes("dmg")
+                );
+                console.log("releaseAsset for", os, releaseAsset);
+                break;
+            case "windows":
+                releaseAsset = latestRelease.assets.find((a) =>
+                    a.name.includes("exe")
+                );
+                break;
+        }
+
+        if (releaseAsset) {
+            btn.href = releaseAsset.browser_download_url;
+            btn.removeAttribute("disabled");
+        }
+    }
+}
+
+if (!window.fetch) {
+    console.error("Browser does not support fetch!");
+} else {
+    getLatestVersion()
+        .then(() => enableDownloadButtons())
+        .catch((err: any) =>
+            console.error("Failed to fetch the latest version", err)
+        );
+}
