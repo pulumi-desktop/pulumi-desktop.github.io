4136784d9e9509d2b88e336ab8ddd8085f6c1cd6
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2022-06-27 18:41:11 -0700

    Update test

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-const platforms = ["linux_x64", "darwin_x64", "windows_x64"];
+const platforms = ["linux_x64", "darwin_x64", "darwin_arm64", "windows_x64"];
 
 test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:1234/");
@@ -12,7 +12,6 @@ test("download buttons are enabled", async ({ page }) => {
     for (let i = 0; i < platforms.length; i++) {
         const p = platforms[i];
         const btn = page.locator(`[data-test=${p}]`);
-        await expect(btn).toContainText(/Download v(.*) for (Linux|macOS|Windows) (arm64|x64)/g);
         await expect(btn).toBeEnabled();
         const link = await btn.getAttribute("href");
         expect(link).toMatch(
